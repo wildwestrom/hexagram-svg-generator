6005dc161f0dfedb3b4dd9a212d53fcdb1ca4428
Christian Westrom <c.westrom@westrom.xyz>
2021-07-05 22:09:28 -0400

    Image can now be resized; Dimensions are now proportional.

--- src/hexagram_svg_generator/frontend.cljs
+++ src/hexagram_svg_generator/frontend.cljs
@@ -10,6 +10,9 @@
 (defonce last-valid-number
   (atom "1"))
 
+(defonce scale-factor
+  (atom "7"))
+
 (defn valid? [num]
   (and (<= 1 num)
        (>= 64 num)
@@ -19,42 +22,97 @@
   [hexagram-number]
   [:div
    [:h2
-    (str "Hexagram #" hexagram-number )
+    (str "Hexagram #" hexagram-number)
     [:br]
     (str "-- " (hex-gen/hexagram-name hexagram-number) " --")]
-   (hexagram-svg hexagram-number)
-   [:h3 (str "Binary Representation: " (hex-gen/hexagram-bin hexagram-number))]])
+   [:h3 (str "Binary Representation: " (hex-gen/hexagram-bin hexagram-number))]
+   [:div {:class "hexagram-svg-container"
+          :style
+          {:width  (* @scale-factor 33)
+           :height "auto"}}
+    (hexagram-svg hexagram-number)]])
+
+(defn scale-slider
+  [min max step]
+  [:div (str
+          (cond
+            (int? step)   "integer"
+            (float? step) "float")
+          " " min "-" max ": ")
+   [:br]
+   [:input {:type      "range"
+            :min       min
+            :max       max
+            :step      step
+            :value     @scale-factor
+            :on-change #(reset! scale-factor (-> % .-target .-value))}]])
+
+(defn number-input
+  [title atom]
+  [:div title
+   [:br]
+   [:input {:type      "text"
+            :value     @atom
+            :on-change #(reset! atom (-> % .-target .-value))}]])
+
+
 
 (defn ui []
-  [:div
+  [:div {:style #js {:padding-left  "1rem"
+                     :padding-right "1rem"
+                     :display       "grid"}}
    [:h1 "I-ching Hexagram Generator"]
-   (if (valid? @hexagram-number)
-     (do (reset! last-valid-number @hexagram-number)
-         (hexagram-info @hexagram-number))
-     (hexagram-info @last-valid-number))
-   [:p "Hexagram number (1-64): "
-    [:input {:type "text"
-             :value @hexagram-number
-             :on-change #(reset! hexagram-number (-> % .-target .-value))}]
-    (if (valid? @hexagram-number)
-      nil
-      [:p "Put in a number from 1-64."])]
-   [:button {:on-click #(reset! hexagram-number (inc (rand-int 64)))} "Generate New Hexagram"]])
+   [:div {:style #js {:display        "flex"
+                      :flex           "0 1 auto"
+                      :flex-direction "row"}}
+    [:div
+     [:p "Scale factor:"]
+     [scale-slider 1 100 1]
+     [scale-slider 0 10 1e-3]
+     [number-input "Any number:" scale-factor]
+     [:br]
+     [number-input "Hexagram number (1-64):" hexagram-number]
+     [:button {:on-click #(reset! hexagram-number (inc (rand-int 64)))} "Generate Random Hexagram"]
+     (if (valid? @hexagram-number)
+       nil
+       [:p {:style #js
+            {:color        "darkred"
+             :border-style "solid"
+             :border-color "red"
+             :padding      "0.5rem"}}
+        "Invalid input:"
+        [:br]
+        "Please put in a number from 1-64."])
+     ]
 
+    [:div {:style #js {:margin-left    "3rem"
+                       :flex-grow      "1"
+                       :padding-left   "1rem"
+                       :padding-right  "1rem"
+                       :padding-bottom "1rem"
+                       :border-style   "solid"
+                       :border-width   "2px"
+                       :border-color   "black"}}
+     (if (valid? @hexagram-number)
+       (do (reset! last-valid-number @hexagram-number)
+           (hexagram-info @hexagram-number))
+       (hexagram-info @last-valid-number))]
+    ]])
 
+(defn mount-root
+  [component]
+  (r.dom/render component (.getElementById js/document "app")))
 
+(defn ^:dev/after-load start
+  []
+  (.log js/console "start")
+  (mount-root [ui]))
 
-(defn start []
-  (r.dom/render [ui]
-                (. js/document (getElementById "app"))))
-
-(defn ^:export init []
-  ;; init is called ONCE when the page loads
-  ;; this is called in the index.html and must be exported
-  ;; so it is available even in :advanced release builds
+(defn ^:export init
+  []
+  (.log js/console "init")
   (start))
 
-(defn stop []
-  ;; stop is called before any code is reloaded
-  ;; this is controlled by :before-load in the config
-  (js/console.log "stop"))
+(defn ^:dev/before-load stop
+  []
+  (.log js/console "stop"))
--- src/hexagram_svg_generator/hex_gen.cljs
+++ src/hexagram_svg_generator/hex_gen.cljs
@@ -1,6 +1,7 @@
 (ns hexagram-svg-generator.hex-gen
   (:require [shadow.resource]
-            [clojure.edn :as edn]))
+            [clojure.edn :as edn]
+            [clojure.string :as string]))
 
 (def all-hexagrams
   (edn/read-string (shadow.resource/inline "./hexagrams.edn")))
@@ -19,38 +20,75 @@
 (defn hexagram-name [num]
   (:name (current-hexagram num)))
 
+(def proportions
+  (let [canvas                     33
+        min-x                      0
+        min-y                      0
+        width                      canvas
+        height                     canvas
+        line-height                (/ height 11)
+        vertical-gap-between-lines (* 2 line-height)
+        yin-line-gap               (/ width 6.6)
+        line-offset-x              min-x
+        yin-line-width             (- (/ width 2)
+                                      (/ yin-line-gap 2))
+        line-offset-x-rightyin     (+ yin-line-width yin-line-gap)
+        line-offset-y              (- width line-height)]
+    {:min-x                      min-x
+     :min-y                      min-y
+     :width                      width
+     :height                     height
+     :line-height                line-height
+     :vertical-gap-between-lines vertical-gap-between-lines
+     :yin-line-gap               yin-line-gap
+     :yin-line-width             yin-line-width
+     :line-offset-x              line-offset-x
+     :line-offset-x-rightyin     line-offset-x-rightyin
+     :line-offset-y              line-offset-y}))
+
 (defn yang
-  [n]
-  ^{:key n}
-  [:rect {:id (str "r-line" n "-yang")
-          :width 66 :height 6
-          :x 2 :y 62}])
+  [line-number]
+  ^{:key line-number}
+  [:rect
+   {:id    (str "line-" (inc line-number) "-yang")
+    :width (:width proportions)         :height (:line-height proportions)
+    :x     (:line-offset-x proportions) :y      (:line-offset-y proportions)}])
 
 (defn yin
-  [n]
-  ^{:key n}
+  [line-number]
+  ^{:key line-number}
   [:g
-   [:rect {:id (str "r-line" n "yinleft")
-           :width 28 :height 6
-           :x 2 :y 62}]
-   [:rect {:id (str "r-line" n "yinright")
-           :width 28 :height 6
-           :x 40 :y 62}]])
+   [:rect {:id    (str "line-" (inc line-number) "-yin-left")
+           :width (:yin-line-width proportions) :height (:line-height proportions)
+           :x     (:line-offset-x proportions)  :y      (:line-offset-y proportions)}]
+   [:rect {:id    (str "line-" (inc line-number) "-yin-right")
+           :width (:yin-line-width proportions)         :height (:line-height proportions)
+           :x     (:line-offset-x-rightyin proportions) :y      (:line-offset-y proportions)}]])
 
 (defn hexagram-svg
   [hexagram-number]
-  (let [num hexagram-number
+  (let [num   hexagram-number
         lines (for [n (range 6)]
                 (nth (map int (seq (hexagram-bin num))) n))]
-    [:div
-     [:svg {:id (hexagram-id num)
-            :width 70
-            :height 70
-            :class "hexagram"}
-      [:g {:id "g-hexagram"}
-       (for [n (range 6)]
-         [:g {:id (str "g-line" (inc n))
-              :transform (str "translate(0," (* n -12) ")")}
-          (condp = (nth lines n)
-            0 (yin n)
-            1 (yang n))])]]]))
+    [:svg {:id                  (hexagram-id num)
+           :viewBox             (string/join
+                      " "
+                      [(:min-x proportions)
+                       (:min-y proportions)
+                       (:width proportions)
+                       (:height proportions)])
+           :preserveAspectRatio "xMidYMid meet"
+           :version             "1.1"
+           :xmlns               "http://www.w3.org/2000/svg"
+           :class               "hexagram"}
+     [:g {:id "g-hexagram"}
+      (for [line-num (range 6)]
+        ^{:key (str "g-line-" (inc line-num))}
+        [:g {:id        (str "g-line-" (inc line-num))
+             :transform (str "translate(0,"
+                             (* line-num
+                                (- (:vertical-gap-between-lines proportions)))
+                             ")")}
+         (condp = (nth lines line-num)
+           0 (yin line-num)
+           1 (yang line-num))])]]))
